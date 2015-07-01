2df0e25ac13f05aa0169577d288fa4a9e8e50639
Bastiaan van der Peet <bas_vanderpeet@yahoo.co.uk>
2015-07-01 09:50:43 +0200

    rename uart ouput variables; rework uart input code

--- BalanceBot.c
+++ BalanceBot.c
@@ -22,113 +22,109 @@
 #define UART_BAUDRATE 38400
 #define UART_TICKS_PER_BIT (CLOCK_FREQ / UART_BAUDRATE + 9) // +9: hand tuned value for 38400 bps
 
-void InitialiseUSI (void) {
-	//pinMode(DataIn, INPUT);         // Define DI as input
-	USICR = 0;                      // Disable USI.
-	GIFR = 1<<PCIF;                 // Clear pin change interrupt flag.
-	GIMSK |= 1<<PCIE1;               // Enable pin change interrupts
-	PCMSK1 |= 1<<PCINT0;             // Enable pin change on pin 0
-}
-
-
-
-unsigned char ReverseByte (unsigned char x) {
-	x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa);
-	x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc);
-	x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0);
-	return x;
-}
-
-
-
 #define UART_TX PORTB0
 #define UART_RX PORTB1
 
-//#define COUNT_BITS 0b111
-#define START_BIT 1<<0
-#define STOP_BIT 1<<1
-#define RUN_BIT 1<<2
-#define PRECALC_BIT 1<<3
+//define some uart_status bit masks
+#define OUT_START (1<<0)
+#define OUT_STOP (1<<1)
+#define OUT_RUN (1<<2)
+#define OUT_PRECALC (1<<3)
+#define IN_START (1<<4)
+#define IN_STOP (1<<5)
 
 #define UART_BUFFER_SIZE 16
 
-char uart_status = PRECALC_BIT;
-volatile char uart_buffer[UART_BUFFER_SIZE];
-volatile uint8_t uart_start_idx, uart_buffer_fill;
+char uart_status = OUT_PRECALC;
+volatile char uart_out_buffer[UART_BUFFER_SIZE];
+volatile uint8_t uart_out_start_idx = 0, uart_out_buffer_fill = 0;
+volatile char uart_in_buffer[UART_BUFFER_SIZE];
+volatile uint8_t uart_in_start_idx = 0, uart_in_buffer_fill = 0;
 
 void precalculate_uart_bit() {
 	static uint8_t uart_bit_mask;
-	if (uart_status & START_BIT) {
-		bit_clear(uart_status, START_BIT | PRECALC_BIT);
+	if (uart_status & OUT_START) {
+		bit_clear(uart_status, OUT_START | OUT_PRECALC);
 		uart_bit_mask = 1;
 	}
-	else if (uart_status & STOP_BIT) {
-		bit_set(uart_status, PRECALC_BIT);
-		bit_clear(uart_status, STOP_BIT);
-		if (uart_buffer_fill == 0) {
-			bit_clear(uart_status, RUN_BIT);
+	else if (uart_status & OUT_STOP) {
+		bit_set(uart_status, OUT_PRECALC);
+		bit_clear(uart_status, OUT_STOP);
+		if (uart_out_buffer_fill == 0) {
+			bit_clear(uart_status, OUT_RUN);
 		}
 		else {
-			uart_buffer_fill--;
-			uart_start_idx = (uart_start_idx + 1) % UART_BUFFER_SIZE;
-			bit_set(uart_status, START_BIT);
+			uart_out_buffer_fill--;
+			uart_out_start_idx = (uart_out_start_idx + 1) % UART_BUFFER_SIZE;
+			bit_set(uart_status, OUT_START);
 		}
 	}
 	else {  // data bits
-		bit_write(uart_buffer[uart_start_idx] & uart_bit_mask , uart_status, PRECALC_BIT);
+		bit_write(uart_out_buffer[uart_out_start_idx] & uart_bit_mask , uart_status, OUT_PRECALC);
 		uart_bit_mask <<= 1;
 		if(!uart_bit_mask)
-		bit_set(uart_status, STOP_BIT);
+		bit_set(uart_status, OUT_STOP);
 	}
 }
 
 
 // some interrupt handlers for input UART
+// working in a nutshell:
+// high->low transition: this is the start bit; triggers interrupt
+// timer samples 8 databits at half- clock offset ()
+// stop bit is ignored
+
 ISR (PCINT_vect) {	// pin change
-	if (!(PINB & 1<<UART_TX)) {       // Ignore if DI is high
-		bit_clear(GIMSK , 1<<PCIE1);          // Disable pin change interrupts
-		OCR0B += UART_TICKS_PER_BIT / 2;
-		TIFR |= 1<<OCF0B;             // Clear output compare flag
-		TIMSK |= 1<<OCIE0B;           // Enable output compare interrupt
+	if (!(PINB & 1<<UART_RX)) {			// we're interested in the falling edge
+		bit_clear(PCMSK1, 1<<UART_RX);	// disable pin change interrupt on RX pin
+		OCR0B += UART_TICKS_PER_BIT / 2;// delay half a uart clock tick
+		bit_set(TIFR, 1<<OCF0B);		// clear timer compare flag
+		bit_set(uart_status, IN_START);
+		bit_set(TIMSK, 1<<OCIE0B);		// enable timer0 compareB interrupt
 	}
 }
 
-ISR (USI_OVF_vect) {	// USI overflow
-	USICR = 0;                      // Disable USI
-	int temp = USIDR;
-	//Display(ReverseByte(temp));
-	GIFR = 1<<PCIF;                 // Clear pin change interrupt flag.
-	GIMSK |= 1<<PCIE1;               // Enable pin change interrupts again
-}
-
-
+// timer interrupt for UART input
 ISR (TIMER0_COMPB_vect) {
-	TIMSK &= ~(1<<OCIE0B);          // Disable COMPB interrupt
-	OCR0B += UART_TICKS_PER_BIT / 2;
-	// Enable USI OVF interrupt, and select Timer0 compare match as USI Clock source:
-	USICR = 1<<USIOIE | 0<<USIWM0 | 1<<USICS0;
-	USISR = 1<<USIOIF | 8;          // Clear USI OVF flag, and set counter
+	static uint8_t bit_mask;
+	OCR0B += UART_TICKS_PER_BIT;
+	if(uart_status & IN_START) {	// start bit
+			bit_clear(uart_status, IN_START);
+			bit_mask = 1;
+			return;
+	}
+	if (uart_status & IN_STOP) {
+		bit_clear(uart_status, IN_STOP);
+		bit_clear(TIMSK, 1<<OCIE0B);	// disable timer0 compareB
+		bit_set(PCMSK1, 1<<UART_RX);	// Enable pin change interrupt on RX pin
+		uart_out_buffer_fill++;
+		return;
+	}
+	bit_write(PORTB & UART_RX, uart_in_buffer[uart_in_start_idx + uart_in_buffer_fill % UART_BUFFER_SIZE], bit_mask);
+	bit_mask <<= 1;
 }
 
+// timer interrupt for UART output
 ISR(TIMER0_COMPA_vect){
 		OCR0A += UART_TICKS_PER_BIT;
-		bit_write(uart_status & PRECALC_BIT, PORTB, 1<<UART_TX);
-		if (uart_status & RUN_BIT) {
+		bit_write(uart_status & OUT_PRECALC, PORTB, 1<<UART_TX);
+		if (uart_status & OUT_RUN) {
 			sei();
 			precalculate_uart_bit();
 		}
 }
+
 uint8_t putstring(char string[]) {
 	int i=0;
-	while (string[i] != '\0' && i < UART_BUFFER_SIZE - uart_buffer_fill - 2) {
-		uart_buffer[(uart_start_idx + uart_buffer_fill + i) % UART_BUFFER_SIZE] = string[i];
+	while (string[i] != '\0' && i < UART_BUFFER_SIZE - uart_out_buffer_fill - 2) {
+		uart_out_buffer[(uart_out_start_idx + uart_out_buffer_fill + i) % UART_BUFFER_SIZE] = string[i];
 		i++;
 	}
-	uart_buffer[(uart_start_idx + uart_buffer_fill + i) % UART_BUFFER_SIZE] = '\r';
+	uart_out_buffer[(uart_out_start_idx + uart_out_buffer_fill + i) % UART_BUFFER_SIZE] = '\r';
 	i++;
-	uart_buffer[(uart_start_idx + uart_buffer_fill + i) % UART_BUFFER_SIZE] = '\n';
-	uart_buffer_fill += i;
-	bit_set(uart_status, RUN_BIT | START_BIT);
+	uart_out_buffer[(uart_out_start_idx + uart_out_buffer_fill + i) % UART_BUFFER_SIZE] = '\n';
+	uart_out_buffer_fill += i;
+	bit_set(uart_status, OUT_RUN | OUT_START);
 	return i;
 }
 
@@ -142,19 +138,17 @@ void uart_setup() {
 	//bit_clear(DDRB, 1<<UART_RX);
 
 	// setup timer0 compare
-
 	OCR0A = UART_TICKS_PER_BIT;
 	bit_clear(TIFR, 1<<OCF0A);	// clear interrupt bit
 	bit_set(TIMSK, 1<<OCIE0A);	// enable compare
 	
 	// input setup	
 	bit_set(TCCR0A, 1<<CS00);	// no prescaling; 8MHz clock
-	OCR0B = OCR0A / 2;			// run at half bit interval
+//	OCR0B = OCR0A / 2;			// run at half bit interval
 
-	USICR = 0;                      // Disable USI.
-	GIFR = 1<<PCIF;                 // Clear pin change interrupt flag.
-	bit_set(GIMSK, 1<<PCIE1);         // Enable pin change interrupts
-	bit_set(PCMSK1, 1<<UART_RX);    // Enable pin change on RX pin
+	GIFR = 1<<PCIF;					// Clear pin change interrupt flag.
+	bit_set(GIMSK, 1<<PCIE1);		// Enable pin change interrupts
+	bit_set(PCMSK1, 1<<UART_RX);	// Enable pin change interrupt on RX pin
 
 }
 
@@ -190,9 +184,8 @@ int main(void)
 	sei(); // enable interrupts
 	//setupUartOut();
 	
-	
-    while(1) // main loop
-    {
-       
-    }
+	while(1) // main loop
+	{
+
+	}
 }
